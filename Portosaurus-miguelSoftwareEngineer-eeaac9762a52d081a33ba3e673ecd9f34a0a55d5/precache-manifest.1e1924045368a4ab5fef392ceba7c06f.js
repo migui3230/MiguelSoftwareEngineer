ff68f8f5aa0eb26a3752ae80d479f345cf2d3b30
migui3230 <74937076+migui3230@users.noreply.github.com>
2022-05-08 17:17:14 -0400

    Add files via upload

---
+++ Portosaurus-miguelSoftwareEngineer-eeaac9762a52d081a33ba3e673ecd9f34a0a55d5/precache-manifest.1e1924045368a4ab5fef392ceba7c06f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48c17cd8c1e7c9127ec80b5ccd2bb54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87a5647cc28e1b0bc115",
+    "url": "./static/css/2.2c0f828a.chunk.css"
+  },
+  {
+    "revision": "14a82b91f0f9cf735dee",
+    "url": "./static/css/main.326e375d.chunk.css"
+  },
+  {
+    "revision": "87a5647cc28e1b0bc115",
+    "url": "./static/js/2.9ff518b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9ff518b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a82b91f0f9cf735dee",
+    "url": "./static/js/main.b7316b47.chunk.js"
+  },
+  {
+    "revision": "18c309d16c6c37590b3f",
+    "url": "./static/js/runtime-main.3207836a.js"
+  },
+  {
+    "revision": "cd6d11ef068a3f0d483a61b73044e4ea",
+    "url": "./static/media/defaultProfile.cd6d11ef.png"
+  },
+  {
+    "revision": "4ef8954d49b3a7ab17e7438cb365028b",
+    "url": "./static/media/defaultbanner.4ef8954d.png"
+  }
+]);
